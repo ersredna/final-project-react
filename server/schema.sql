10a9fc426927dce29ceb6a92fba79aef339d16e6
Ersredna <ersredna@gmail.com>
2024-02-16 13:38:34 -0800

    started working on database

--- server/schema.sql
+++ server/schema.sql
@@ -1,16 +1,29 @@
--- CREATE TABLE characters (
---     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
---     name TEXT NOT NULL
--- );
--- CREATE TABLE connections (
---     char_id_1 INTEGER NOT NULL,
---     char_id_2 INTEGER NOT NULL,
---     FOREIGN KEY(char_id_1) REFERENCES characters(id),
---     FOREIGN KEY(char_id_2) REFERENCES characters(id)
--- );
--- CREATE TABLE users (
---     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
---     username TEXT NOT NULL,
---     hash TEXT NOT NULL,
---     is_admin BIT NOT NULL DEFAULT 0
--- );
+-- @block
+CREATE TABLE characters (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+-- @block
+CREATE TABLE connections (
+    char_id_1 INT NOT NULL,
+    char_id_2 INT NOT NULL,
+    FOREIGN KEY(char_id_1) REFERENCES characters(id),
+    FOREIGN KEY(char_id_2) REFERENCES characters(id)
+);
+
+-- @block
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    hash VARCHAR(255) NOT NULL,
+    is_admin BIT NOT NULL DEFAULT 0
+);
+
+-- @block
+CREATE TABLE test (
+    name VARCHAR(255) PRIMARY KEY NOT NULL
+);
+
+-- @block
+INSERT INTO test VALUES ('anders');
